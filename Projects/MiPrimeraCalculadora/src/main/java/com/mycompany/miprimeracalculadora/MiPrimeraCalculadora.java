3bcd3b722002cbf020928e3b2def8c80e7f54834
Miguel <perezojedamiguel99@gmail.com>
2025-03-16 17:56:45 -0500

    organizing

---
+++ Projects/MiPrimeraCalculadora/src/main/java/com/mycompany/miprimeracalculadora/MiPrimeraCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprimeracalculadora;
+
+/**
+ *
+ * @author h1dr4
+ */
+public class MiPrimeraCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Projects/MiPrimeraCalculadora/src/main/java/paquete/Prueba.java
@@ -0,0 +1,15 @@
+package paquete;
+
+/**
+ *
+ * @author h1dr4
+ */
+public class Prueba {
+    public static void main(String[] args){
+    
+      System.out.println(18%4);
+                 
+    }
+    
+            
+}
