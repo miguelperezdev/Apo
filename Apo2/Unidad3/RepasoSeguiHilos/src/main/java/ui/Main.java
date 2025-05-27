575449d1d03a2ceeade6ea0e39716c29c802d65f
Miguel Perez Ojeda <1112046069@u.icesi.edu.co>
2025-05-27 09:06:31 -0500

    Talleres

---
+++ Apo2/Unidad3/RepasoSeguiHilos/src/main/java/ui/Main.java
@@ -0,0 +1,26 @@
+package ui;
+import model.*;
+
+public class Main {
+    public static void main (String[] args) {
+        Counter counter = new Counter() ;
+
+        Thread t1 = new Thread(new Task(counter));
+        Thread t3 = new Thread(new Task(counter));
+        Thread t2 = new Thread(new Task(counter));
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Valor final del contador: " + counter.getCount());
+    }
+}
